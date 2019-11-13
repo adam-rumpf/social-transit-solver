13ca51fef2ee397527c43704c5a0644ff60d1a8d
Adam Rumpf <arumpf@hawk.iit.edu>
2019-11-12 19:07:31 -0500

    Adding logger class placeholders.

--- logger.cpp
+++ logger.cpp
@@ -1,1 +1,15 @@
 #include "logger.hpp"
+
+/// Converts a solution vector to a string by simply concatenating its digits separated by underscores.
+string solution_string(const vector<int> &sol)
+{
+	string out = "";
+
+	for (int i = 0; i < sol.size(); i++)
+		out += to_string(sol[i]) + '_';
+	out.pop_back();
+
+	return out;
+}
+
+//////////// When loading the initial solution log, try to open the solution log file. If missing, exit(FILE_NOT_FOUND). If empty, exit(INCORRECT_FILE).
--- logger.hpp
+++ logger.hpp
@@ -1,19 +1,53 @@
 /**
+Logger objects for reading and writing various files required by the main solution algorithm.
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include "definitions.hpp"
+
+using namespace std;
+
+// Function prototypes
+string solution_string(const vector<int> &); // returns string version of integer vector
+
+/**
 Event logger.
 
 Reports the events that occur during each iteration of the search process and prints them to an output log for later review.
 */
+struct EventLog
+{
+	// Public attributes
+
+	// Public methods
+	EventLog() {};
+};
 
 /**
 Memory logger.
 
 Writes the TS/SA algorithm's memory structures to an output file. This allows the search process to be halted and resumed later.
 */
+struct MemoryLog
+{
+	// Public attributes
+
+	// Public methods
+	MemoryLog() {};
+};
 
 /**
 Solution logger.
 
 Records information about every solution encountered during the search process. This allows us to simply look up previously-generated solutions instead of having to reevaluate the objective and constraint functions.
 */
+struct SolutionLog
+{
+	// Public attributes
 
-#pragma once
+	// Public methods
+	SolutionLog() {};
+};
